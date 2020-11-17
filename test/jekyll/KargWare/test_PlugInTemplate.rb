84021fc0ae689fe255d820ddee152e49cd023246
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-11-17 14:44:23 +0100

    rename file to match the test pattern

--- test/jekyll/KargWare/test_PlugInTemplate.rb
+++ test/jekyll/KargWare/test_PlugInTemplate.rb
@@ -2,10 +2,10 @@ require "test_helper"
 
 class Jekyll::KargWare::PlugInTemplateTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Jekyll::KargWare::PlugInTemplate::VERSION
+    refute_nil Jekyll::KargWare::PlugInTemplate::VERSION
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
   end
 end
