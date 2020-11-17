1f62001becfa640907b73074746957710578a0e0
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-11-17 12:18:11 +0100

    Add 'bundle gem' generated files

---
+++ lib/jekyll/KargWare/PlugInTemplate.rb
@@ -0,0 +1,10 @@
+require "jekyll/KargWare/PlugInTemplate/version"
+
+module Jekyll
+  module KargWare
+    module PlugInTemplate
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/KargWare/PlugInTemplate/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module KargWare
+    module PlugInTemplate
+      VERSION = "0.1.0"
+    end
+  end
+end
