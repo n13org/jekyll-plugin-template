48c4249201c9a48f374a058d9055a5eefcf7dc42
Nicolas Karg <50399433+N7K4@users.noreply.github.com>
2020-12-26 10:32:50 +0100

    Add template `Ruby Class With Test`

---
+++ .fttemplates/Ruby Class with Test/lib/jekyll/[Company]/[Project]/[FTName % lowercase].rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module <Company>
+    module <Project>
+      class <FTName | pascalcase>
+      end
+    end
+  end
+end
---
+++ .fttemplates/Ruby Class with Test/test/jekyll/[Company]/[Project]/[FTName % lowercase]_test.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Jekyll
+  module <Company>
+    module <Project>
+      class <FTName | pascalcase>Test < Minitest::Test
+        def test_fail_always
+          assert false
+        end
+      end
+    end
+  end
+end
